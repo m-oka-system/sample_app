b360fdaa9e2e6af0b8935e6dbc31ea1708492017
Makoto Okada <fun-for-fan@outlook.com>
2017-11-04 16:13:59 +0000

    Make a Relationship model

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,6 +1,7 @@
 class MicropostsController < ApplicationController
   before_action :authenticate_user!
   before_action :correct_user, only: :destroy
+  #before_action :micropost_params, only: :create
 
   def create
     @micropost = current_user.microposts.build(micropost_params)
@@ -9,6 +10,7 @@ class MicropostsController < ApplicationController
       redirect_to root_url
     else
       @feed_items = []
+      #redirect_to root_url
       render 'static_pages/home'
     end
   end
@@ -22,6 +24,7 @@ class MicropostsController < ApplicationController
   private
   
     def micropost_params
+      #@micropost = current_user.microposts.build(id: params[:id])
       params.require(:micropost).permit(:content)
     end
     
